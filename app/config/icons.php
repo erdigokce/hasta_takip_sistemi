af89997c4b6892b2fbeef60461eec1bb69f56479
Erdi GÖKCE <erdi.gokce@gmail.com>
2017-05-14 12:54:28 +0300

    National flag images added to navbar langauge selector section.

---
+++ app/config/icons.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$national_flags = array(
+/* code => [image path, width, height, alt] */
+  'en' => array('app/images/flags/flag_uk.png', '24px', '24px', 'United Kingdom National Flag'),
+  'uk' => array('app/images/flags/flag_uk.png', '24px', '24px', 'United Kingdom National Flag'),
+  'us' => array('app/images/flags/flag_us.png', '24px', '24px', 'United States Of America National Flag'),
+  'tr' => array('app/images/flags/flag_tr.png', '24px', '24px', 'Turkish National Flag')
+);

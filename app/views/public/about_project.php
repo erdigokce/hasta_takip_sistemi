6c4e9e36d4698b4235845ad80f5fd28b992e67eb
Erdi GÖKCE <erdi.gokce@gmail.com>
2017-05-16 22:50:04 +0300

    Font sizes rearranged in these public pages.

--- app/views/public/about_project.php
+++ app/views/public/about_project.php
@@ -1,12 +1,12 @@
-<h3 class="text-center text-primary">
+<h2 class="text-center text-primary">
   <?php echo get($home_about_project_title); ?>
-</h3>
+</h2>
 <p class="text-justify text-default">
   <?php echo get($home_about_project_text); ?>
 </p>
-<h4 class="text-center text-primary">
+<h3 class="text-center text-primary">
   <?php echo get($home_about_project_future_title); ?>
-</h4>
+</h3>
 <p class="text-justify text-default">
   <?php echo get($home_about_project_future_text); ?>
 </p>
--- app/views/public/about_system.php
+++ app/views/public/about_system.php
@@ -1,6 +1,6 @@
-<h3 class="text-center text-primary">
+<h2 class="text-center text-primary">
   <?php echo get($home_about_system_title); ?>
-</h3>
+</h2>
 <p class="text-justify text-default">
   <?php echo get($home_about_system_text); ?>
 </p>
